4740f9314a88f6e087e9087be1b171944f55ccf8
Octavio Salcedo <150973163+TavoSalcedoMTZ@users.noreply.github.com>
2026-07-14 20:46:21 -0600

    circulo
    Creamos un area circular donde por el momento podemos matar a los enemigos dentro del area, y tambien uno que usaremos para el debug y poder comprobar los rangos

--- src/main/java/com/hardcore/gamemod/TpCommand.java
+++ src/main/java/com/hardcore/gamemod/TpCommand.java
@@ -5,11 +5,22 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.fabricmc.loader.impl.lib.sat4j.core.Vec;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.commands.Commands;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.world.phys.Vec3;
+import org.apache.logging.log4j.core.jmx.Server;
+import net.minecraft.world.level.block.Blocks;
+
+import java.util.List;
 
 
 public class TpCommand {
@@ -28,7 +39,25 @@ public class TpCommand {
                                         )
                         )
         );
+
+        dispatcher.register(
+                Commands.literal("getpos")
+                        .executes(TpCommand::getPlayerPositionCommand)
+        );
+
+        dispatcher.register(
+                Commands.literal("killCircle")
+                        .executes(TpCommand::drawcircle)
+        );
+
+        dispatcher.register(
+                Commands.literal("drawCircle")
+                        .executes(TpCommand::drawcircleBlock)
+        );
+
+
     }
+
 
     //Metodo para verificar la experiencia del jugador funcionara para poder comprobar si se puede hacer el TP
     private static boolean hasEnoughtXP(Player player){
@@ -64,5 +93,95 @@ public class TpCommand {
         return 1;
     }
 
+    private static void GetPlayerPosition(ServerPlayer player){
+
+        Vec3 position = player.getPosition(1);
+        player.sendSystemMessage(Component.literal("Tu posicion es ."+ position));
+    }
+
+    private static int getPlayerPositionCommand(CommandContext<CommandSourceStack> ctx)
+            throws CommandSyntaxException {
+
+        ServerPlayer player = ctx.getSource().getPlayerOrException();
+
+        GetPlayerPosition(player);
+
+        return 1;
+    }
+
+
+    private static int drawcircle(CommandContext<CommandSourceStack> ctx)
+            throws CommandSyntaxException {
+
+        ServerPlayer player = ctx.getSource().getPlayerOrException();
+
+        Vec3 center= player.getPosition(1);
+        double radius = 10;
+
+        AABB area = new AABB(
+
+                center.x - radius,
+                center.y - radius,
+                center.z -radius,
+
+                center.x + radius,
+                center.y + radius,
+                center.z + radius
+        );
+
+
+        ServerLevel level = player.serverLevel();
+
+        List<LivingEntity> entities = level.getEntitiesOfClass(LivingEntity.class, area);
+
+
+        for(Entity entity: entities){
+            double dx = entity.getX() - center.x;
+            double dz = entity.getZ() - center.z;
+
+            if(dx * dx + dz * dz <= radius * radius) {
+
+                if (entity == player)
+                    continue;
+                entity.kill();
+            }
+
+
+        }
+
+        return 1;
+    }
+
+    private static int drawcircleBlock(CommandContext<CommandSourceStack> ctx)
+            throws CommandSyntaxException {
+
+        ServerPlayer player = ctx.getSource().getPlayerOrException();
+
+        Vec3 center= player.getPosition(1);
+        int radius = 10;
+        ServerLevel level = player.serverLevel();
+
+        int centerX = (int)Math.floor(center.x);
+        int centerY = (int)Math.floor(center.y);
+        int centerZ = (int)Math.floor(center.z);
+
+
+        for(int x = centerX - radius; x<= centerX+radius; x++){
+            for(int z = centerZ - radius; z <=centerZ+radius; z++){
+
+
+                double dx = x-centerX;
+                double dZ = z-centerZ;
+
+                if(dx * dx + dZ * dZ <=radius * radius){
+
+                    BlockPos pos = new BlockPos(x, centerY , z);
+
+                    level.setBlock(pos, Blocks.REDSTONE_BLOCK.defaultBlockState(),3);
+                }
+            }
+        }
+        return 1;
+    }
 
 }
